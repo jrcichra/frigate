98efa595b4fa3ef196d52e43cfbe42dbdf77dc96
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2026-04-25 18:02:16 -0400

    fix: review player hour boundary break and infinite spinner
    - Add force_discontinuity=true to VOD playlist URL so HLS gets
      proper discontinuity markers between hourly recording segments,
      preventing playback from stalling at hour boundaries
    - Clear isLoading/isBuffering in onPlaying so spinner always
      dismisses when video starts playing, not just on timeupdate

--- web/src/components/player/dynamic/DynamicVideoPlayer.tsx
+++ web/src/components/player/dynamic/DynamicVideoPlayer.tsx
@@ -271,7 +271,7 @@ export default function DynamicVideoPlayer({
     }
 
     setSource({
-      playlist: `${apiHost}vod/${camera}/start/${recordingParams.after}/end/${recordingParams.before}/master.m3u8`,
+      playlist: `${apiHost}vod/${camera}/start/${recordingParams.after}/end/${recordingParams.before}/master.m3u8?force_discontinuity=true`,
       startPosition,
     });
 
@@ -360,6 +360,8 @@ export default function DynamicVideoPlayer({
               clearTimeout(loadingTimeout);
             }
 
+            setIsLoading(false);
+            setIsBuffering(false);
             setNoRecording(false);
           }}
           setFullResolution={setFullResolution}
